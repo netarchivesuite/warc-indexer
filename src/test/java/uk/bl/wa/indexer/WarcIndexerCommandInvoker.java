06361bbaae9e4050a3b5aa45faff7af88064a053
Asger Askov Blekinge <asga@kb.dk>
2025-07-17 10:31:54 +0200

    Command invoker for tests

---
+++ src/test/java/uk/bl/wa/indexer/WarcIndexerCommandInvoker.java
@@ -0,0 +1,16 @@
+package uk.bl.wa.indexer;
+
+import javax.xml.transform.TransformerException;
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class WarcIndexerCommandInvoker {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException, TransformerException {
+        WARCIndexerCommand.main(new String[]{
+                "-c", "conf/config3.conf",
+                "-s", "http://localhost:8983/solr/netarchivebuilder",
+                new File(Thread.currentThread().getContextClassLoader().getResource("IAH-20080430204825-00000-blackbook-truncated.warc.gz").getFile()).getAbsolutePath()
+        });
+    }
+}
